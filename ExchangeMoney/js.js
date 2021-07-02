e4299a561707597a0a8f2f1bc8381659be129e1d
Peranut Boonyakit <peranut.pepo@hotmail.com>
2021-07-02 13:49:13 +0700

    calculate

--- ExchangeMoney/js.js
+++ ExchangeMoney/js.js
@@ -8,12 +8,24 @@ const amount_two = document.getElementById('amount_two');
 const swap = document.getElementById('btn')
 const rateText = document.getElementById('rate');
 
-currency_one.addEventListener('change', calculateMoney)
-currency_two.addEventListener('change', calculateMoney)
+currency_one.addEventListener('change', calculateMoney);
+currency_two.addEventListener('change', calculateMoney);
+amount_one.addEventListener('input', calculateMoney);
+amount_two.addEventListener('input', calculateMoney);
 
 function calculateMoney(){
     const one = currency_one.value;
     const two = currency_two.value;
-    let url = `https://api.exchangerate-api.com/v4/latest/${one}`
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${one}`)
+    .then(res=>res.json())
+    .then(data=>{
+        const rate = data.rates[two]; //Exchange Rate
+
+        rateText.innerText = ` 1 ${one} = ${rate} ${two}`
+
+        amount_two.value = amount_one.value*rate
+    })
 }
+
 calculateMoney();
