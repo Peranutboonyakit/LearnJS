9da2057a978334404ad563787570f910e1d38bc6
peranut <peranut.pepo@hotmail.com>
2021-07-02 21:04:18 +0700

    done

--- ExchangeMoney/js.js
+++ ExchangeMoney/js.js
@@ -1,17 +1,21 @@
-
+//Exchange
 const currency_one = document.getElementById('currency_one');
 const currency_two = document.getElementById('currency_two');
 
+//Input
 const amount_one = document.getElementById('amount_one');
 const amount_two = document.getElementById('amount_two');
 
+//Swap & Rate
 const swap = document.getElementById('btn')
 const rateText = document.getElementById('rate');
 
+//Event
 currency_one.addEventListener('change', calculateMoney);
 currency_two.addEventListener('change', calculateMoney);
 amount_one.addEventListener('input', calculateMoney);
 amount_two.addEventListener('input', calculateMoney);
+swap.addEventListener('click', swapMoney);
 
 function calculateMoney(){
     const one = currency_one.value;
@@ -26,6 +30,13 @@ function calculateMoney(){
 
         amount_two.value = (amount_one.value*rate).toFixed(2)
     })
+}
+
+function swapMoney(){
+    const first = currency_one.value; // Frist Exchange
+    currency_one.value = currency_two.value;
+    currency_two.value = first;
+    calculateMoney();
 }
 
 calculateMoney();
